a2c25d1684ceeaf6a62f0828c3b3322ab4ae8a48
shovan777 <raj.shrestha778@gmail.com>
2018-03-18 14:24:41 +0545

    done

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -14,7 +14,7 @@ var encoded_pass = encodeURIComponent(password);
 var connectPath, options;
 //Check if we are on Heroku
 if(process.env.PORT){
- connectPath = "mongodb://ds117719.mlab.com:17719/todoapp";
+ connectPath = "mongodb://raj778@ds117719.mlab.com:17719/todoapp";
  options= {
      auth: {
          user: username,
