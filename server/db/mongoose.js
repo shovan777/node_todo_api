388699db925fc898fab6c3c735a5bad9cf5aec45
shovan777 <raj.shrestha778@gmail.com>
2018-03-18 14:19:57 +0545

    done

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,27 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 var username = 'raj778';
+var encoded_user = encodeURIComponent(username);
 var password = 'r@j777';
+var encoded_pass = encodeURIComponent(password);
+// var connectPath = `mongodb://${encoded_user}:${encoded_pass}@ds117719.mlab.com:17719/todoapp`;
+// connectPath = encodeURIComponent(connectPath);
 
-var connectPath = `mongodb://${username}:r%40j777@ds117719.mlab.com:17719/todoapp`;
-// connectPath = null;
-mongoose.connect(connectPath || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(connectPath || 'mongodb://localhost:27017/TodoApp');
+
+var connectPath, options;
+//Check if we are on Heroku
+if(process.env.PORT){
+ connectPath = "mongodb://ds117719.mlab.com:17719/todoapp";
+ options= {
+     auth: {
+         user: username,
+         password: password
+     }
+ }
+}else{
+ connectPath = "mongodb://localhost:27017/TodoApp";
+ options = {}
+}
+
 module.exports = {mongoose};
