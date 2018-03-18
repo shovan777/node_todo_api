9a72f2ca64e9cd4e20106238fc5486a730507f17
shovan777 <raj.shrestha778@gmail.com>
2018-03-18 13:44:28 +0545

    modify mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,19 +2,7 @@ var mongoose = require('mongoose');
 // why did he make a mongoose variable here and not a constant
 
 mongoose.Promise = global.Promise;
-var connectPath, options;
-//Check if we are on Heroku
-if(process.env.PORT){
- connectPath = 'mongodb://raj778:<dbpassword>@ds117719.mlab.com:17719/todoapp';
- options= {
-     auth: {
-         user: 'raj778',
-         password: 'r@j777'
-     }
- }
-}else{
- connectPath = "mongodb://localhost:27017/TodoApp";
- options = {}
-}
-mongoose.connect(connectPath, options);
+
+var connectPath = 'mongodb://raj778:r@j777@ds117719.mlab.com:17719/todoapp';
+mongoose.connect(connectPath || 'mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
