356b25c01fd3ac8bca41e389170be64be5ec597c
shovan777 <raj.shrestha778@gmail.com>
2018-03-18 13:37:14 +0545

    connect to mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,19 @@ var mongoose = require('mongoose');
 // why did he make a mongoose variable here and not a constant
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+var connectPath, options;
+//Check if we are on Heroku
+if(process.env.PORT){
+ connectPath = 'mongodb://raj778:<dbpassword>@ds117719.mlab.com:17719/todoapp';
+ options= {
+     auth: {
+         user: 'raj778',
+         password: 'r@j777'
+     }
+ }
+}else{
+ connectPath = "mongodb://localhost:27017/TodoApp";
+ options = {}
+}
+mongoose.connect(connectPath, options);
 module.exports = {mongoose};
