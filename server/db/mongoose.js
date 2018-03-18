1a8f84dc72b9ae88cbeb55b5aef661054a49c411
shovan777 <raj.shrestha778@gmail.com>
2018-03-18 14:00:55 +0545

    modify mongooseklfvdsjxskl

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 // why did he make a mongoose variable here and not a constant
 
 mongoose.Promise = global.Promise;
+var username = 'raj778';
+var password = 'r@j777';
 
-var connectPath = 'mongodb://raj778:r@j777@ds117719.mlab.com:17719/todoapp';
+var connectPath = `mongodb://${username}:r%40j777@ds117719.mlab.com:17719/todoapp`;
+// connectPath = null;
 mongoose.connect(connectPath || 'mongodb://localhost:27017/TodoApp');
 module.exports = {mongoose};
